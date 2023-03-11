55bde62caa759c7c2a3bb027666f82d95023592a
Sonder <814029073@qq.com>
2023-03-11 11:14:42 +0800

    1

---
+++ key.php
@@ -0,0 +1,3 @@
+<?php
+
+return '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';
--- stream.php
+++ stream.php
@@ -1,12 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: text/event-stream");
-
+$key = require_once 'key.php';
+//$key = file_get_contents(__DIR__ . "/key"); // 读取文件内容
 session_start();
 $postData = $_SESSION['data'];
 $_SESSION['response'] = "";
 $ch = curl_init();
-$OPENAI_API_KEY = "";
+$OPENAI_API_KEY = $key;
 if ((isset($_SESSION['key'])) && (!empty($_POST['key']))) {
     $OPENAI_API_KEY = $_SESSION['key'];
 }
